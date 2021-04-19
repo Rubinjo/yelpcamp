670d8d88f7d3be28ee7f06214abb59a8a2608573
Ruben <ruben.lucas1997@gmail.com>
2021-04-19 22:40:13 +0200

    ✨ Image uploading

--- seeds/index.js
+++ seeds/index.js
@@ -28,10 +28,21 @@ const seedDB = async () => {
       author: "604a4201e727605d04a05df1",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       price: price,
+      images: [
+        {
+          url:
+            "https://res.cloudinary.com/dd1m9akdn/image/upload/v1618840094/YelpCamp/joughki7bi1vw3d5w76s.jpg",
+          filename: "YelpCamp/joughki7bi1vw3d5w76s",
+        },
+        {
+          url:
+            "https://res.cloudinary.com/dd1m9akdn/image/upload/v1618840095/YelpCamp/miaxweox2csnzddetexc.jpg",
+          filename: "YelpCamp/miaxweox2csnzddetexc",
+        },
+      ],
     });
     await camp.save();
   }
